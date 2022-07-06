a71feb0a1a202447f0fc123f73198563906b61f9
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-07-06 15:32:08 +0100

    TASK-58038: Add based swagger rest documentation for social and gatein-portal (#320)
    Add based swagger rest documentation for social and gatein-portal

--- docs/Swagger_Rest_Apis.rst
+++ docs/Swagger_Rest_Apis.rst
@@ -6,6 +6,7 @@ eXo Rest APIs
 
  This chapter includes an overview to eXo Rest APIs definitions.
 
+<<<<<<< HEAD
  :ref:`Agenda Addon Rest API <AGENDA_ADDON_REST_API>`
 
  :ref:`Anti-Malware Addon Rest API <ANTI_MALWARE_ADDON_REST_API>`
@@ -247,6 +248,11 @@ eXo Rest APIs
 
  .. raw:: html
    :file: embed/poll-docs-swagger.html
+=======
+ :ref:`Social Rest API <SOCIAL_REST_API>`
+
+ :ref:`Gatein-Portal Rest API <GATEIN_PORTAL_REST_API>`
+>>>>>>> 1e7dff4 (TASK-58038: Add based swagger rest documentation for social and gatein-portal (#320))
 
 
 .. _SOCIAL_REST_API:
@@ -254,13 +260,18 @@ eXo Rest APIs
   **Social Rest Api**
 
   The eXo Social REST APIs provide service endpoints for listing, creating, updating and deleting entities related to Social functionality.
+<<<<<<< HEAD
 
   Following the list of eXo Social API operations :
 
+=======
+  Following the list of eXo Social API operations : 
+>>>>>>> 1e7dff4 (TASK-58038: Add based swagger rest documentation for social and gatein-portal (#320))
 
  .. raw:: html
    :file: embed/social-docs-swagger.html
 
+<<<<<<< HEAD
 .. _TASKS_ADDON_REST_API:
 
   **Tasks Addon Rest Api**
@@ -295,3 +306,14 @@ eXo Rest APIs
    :file: embed/web-conferencing-docs-swagger.html
 
 
+=======
+.. _GATEIN_PORTAL_REST_API:
+
+  **Gatein-portal Rest Api**
+
+  The eXo Portal REST APIs provide service endpoints for listing, creating, updating and deleting entities related to Portal layer functionality.
+  Following the list of eXo Portal API operations : 
+
+ .. raw:: html
+   :file: embed/gatein-portal-docs-swagger.html
+>>>>>>> 1e7dff4 (TASK-58038: Add based swagger rest documentation for social and gatein-portal (#320))
--- docs/_static/js/DocsUtils.js
+++ docs/_static/js/DocsUtils.js
@@ -1,7 +1,48 @@
+function getBuildVersion(url) {
+    return new Promise((resolve, reject) => {
+        let buildVersion = '';
+        const specUrl = url + "/maven-metadata.xml";
+        $.ajax({
+            type: "get",
+            url: specUrl,
+            dataType: "xml",
+            success: function (data) {
+                let versions = data.getElementsByTagName('versioning')[0].getElementsByTagName('snapshotVersion');
+                for (let i = 0; i < versions.length; i++) {
+                    const classifier = versions[i].getElementsByTagName('classifier')[0]?.innerHTML;
+                    const extension = versions[i].getElementsByTagName('extension')[0]?.innerHTML
+                    const value = versions[i].getElementsByTagName('value')[0]?.innerHTML
+                    if (classifier === 'swagger' && extension === 'json') {
+                        buildVersion = value;
+                        break;
+                    }
+                }
+                resolve(buildVersion)
+            },
+            error: function (error) {
+                reject(error);
+            }
+        })
+    })
+}
+
 function swaggerUi(options) {
+    let url = options.url + options.restModule + '/' + options.version;
+    if (options.version.endsWith("-SNAPSHOT")) {
+        getBuildVersion(url).then(buildVersion => {
+            url = url + '/' + options.restModule + "-" + buildVersion;
+            initSwaggerUIBundle(options.domId, url);
+        });
+    } else {
+        url = url + '/' + options.restModule + "-" + options.version;
+        initSwaggerUIBundle(options.domId, url);
+    }
+}
+
+function initSwaggerUIBundle(domId, url) {
     window.ui = SwaggerUIBundle({
-        url: options.url ,
-        dom_id: options.domId,
+        url: url + "-swagger.json",
+        dom_id: domId,
         deepLinking: true,
         presets: [
             SwaggerUIBundle.presets.apis,
@@ -12,5 +53,4 @@ function swaggerUi(options) {
         ],
         layout: "BaseLayout"
     });
-
 }
